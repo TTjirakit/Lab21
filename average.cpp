77f8cbcdfe092e9f405c39ebbfcf3962742d5a8f
TTjirakit <captain112006@gmail.com>
2025-02-19 22:05:45 +0700

    averge.exe complete

--- average.cpp
+++ average.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
 
+int main(int argc, char* argv[]) {
+    if (argc == 1) {
+        cout << "Please input numbers to find average.\n";
+        return 0;
+    }
+
+    int sum = 0, count = argc - 1;
+    for (int i = 1; i < argc; ++i) {
+        int num = atoi(argv[i]);
+        sum += num;
+    }
+
+    double average = (double)sum / count;
+    
+    cout << "---------------------------------\n";
+    cout << "Average of " << count << " numbers = " << average << "\n";
+    cout << "---------------------------------\n";
+
+    return 0;
+}
